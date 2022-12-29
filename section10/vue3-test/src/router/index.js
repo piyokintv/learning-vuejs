0ee3d64e7a17039e157519514b10f74dd2dbbe6c
piyokintv <kmori0050@gmail.com>
2022-12-30 03:41:59 +0900

    Provide/Inject 長距離Props

--- section10/vue3-test/src/router/index.js
+++ section10/vue3-test/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ChildrenView from '@/views/ChildrenView'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/children',
+    name: 'Children',
+    component: ChildrenView
   },
   {
     path: '/about',
---
+++ section10/vue3-test/src/views/ChildrenView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Children（子）</div>
+  <Grand-Children />
+</template>
+
+<script>
+import GrandChildren from '@/components/GrandChildren'
+
+export default {
+  components: {
+    GrandChildren
+  }
+
+}
+</script>
+
+<style>
+
+</style>
