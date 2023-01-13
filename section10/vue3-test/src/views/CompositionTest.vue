eb1bd385e67af20dba83b2714b30ce79e58e7683
piyokintv <kmori0050@gmail.com>
2023-01-13 15:41:15 +0900

    ref()

--- section10/vue3-test/src/views/CompositionTest.vue
+++ section10/vue3-test/src/views/CompositionTest.vue
@@ -3,21 +3,29 @@
     CompositionTest
     <p>{{ name }}</p>
     <p>{{ age }}</p>
+    <p>{{ nameRef }}</p>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
   setup() {
     let name = '大谷';
     const age = 30;
 
+    const nameRef = ref('錦織');
+
     console.log('setup');
     console.log(this);
+    console.log(nameRef);
+    console.log(nameRef.value);
 
     return {
       name,
       age,
+      nameRef,
     }
   },
   data() {
