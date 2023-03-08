73ce8c2d49d6dc74b4f2f19556f46aa0275d5116
piyokintv <kmori0050@gmail.com>
2023-03-08 14:28:33 +0900

    setup内でvue-router

---
+++ section10/vue3-test/src/views/RouterTest.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    RouterTest
+  </div>
+  <button @click="goHome">ホームに戻る</button>
+  <button @click="checkRoutePath">ルート情報</button>
+</template>
+
+<script>
+import { useRouter, useRoute } from 'vue-router';
+
+export default {
+  setup() {
+    const router = useRouter();
+    const route = useRoute();
+
+    const goHome = () => {
+      router.push('/');
+    };
+
+    const checkRoutePath = () => {
+      console.log(route.path);
+    };
+
+    return {
+      goHome,
+      checkRoutePath,
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
